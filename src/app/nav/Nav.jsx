e79f907429e25f8e8595e84099d69b776a3c50e9
Mohd-Akbar1 <34mohdakbar@gmail.com>
2025-05-23 23:13:19 +0530

    navbar

--- src/app/nav/Nav.jsx
+++ src/app/nav/Nav.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import Image from 'next/image'
+import React, { useState } from 'react';
+import Image from 'next/image';
 import { HiMagnifyingGlass } from "react-icons/hi2";
+
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+
   return (
     <div className='nav  flex flex-col  pt-4 relative '>
-      <div className='w-[90%] border-2 mx-auto  md:flex rounded-full border-white/40 gap-2' >
+      <div className='hidden w-[90%] border-2 mx-auto  md:flex rounded-full border-white/40 gap-2' >
        <Image src="/image1.png" alt="Logo" width={50} height={50} className='rounded-full animate-spin' />
         <div className='flex flex-1 justify-between'>
            
@@ -27,6 +31,24 @@ const Nav = () => {
         </div>
         
 
+      </div>
+      <div className='flex md:hidden w-[90%] border-2 mx-auto  justify-between rounded-full border-white/40 gap-2'>
+               <Image src="/image1.png" alt="Logo" width={50} height={50} className='rounded-full animate-spin' />
+               <div className='flex md:hidden items-center w-[90%] mx-auto justify-between border-2 border-white/40 rounded-full px-4 py-2'>
+        <Image src="/image1.png" alt="Logo" width={40} height={40} className='rounded-full animate-spin' />
+        <button className='text-3xl' onClick={toggleMenu}>
+          {isOpen ? '❌' : '🌿'}
+        </button>
+      </div>
+
+
+      </div>
+      <div className='flex flex-col md:hidden text-white w-full justify-between items-center gap-2 px-2'>
+       
+          <button className='w-full border-1 border-white rounded-md text-lg font-bold '>Home</button>
+          <button className='w-full border-1 border-white rounded-md text-lg font-bold' >Blogs</button>
+          <button className='w-full border-1 border-white rounded-md text-lg font-bold'>Contacts</button>
+        
       </div>
 
       <div className='w-[20vw] bg-white  absolute bottom-0 right-0 rounded-t-lg'>
