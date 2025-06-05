f3b09621436796743e21789c672e39b9c8c93746
Mohd-Akbar1 <34mohdakbar@gmail.com>
2025-06-05 12:56:47 +0530

    updated details in indoor plants

--- src/app/Indoor.jsx
+++ src/app/Indoor.jsx
@@ -11,15 +11,38 @@ const Indoor = () => {
 
         {/* plants section */}
 
-        <div className='w-[80vw] mx-auto grid grid-cols-5 grid-rows-2 gap-4'>
+        <div className='w-[80vw] mx-auto grid grid-cols-5 grid-rows-2 gap-4 m-2 p-4'>
 
-            <div className='border-1 border-gray-300 rounded-xl shadow-md grid-cols-3'>
-                <p className='text-4xl font-bold'>Philodendron</p>
+        {/* top div */}
+            <div className='border-1 bg-[url("/plant1.jpg")] text-white  justify-end min-h-[30vh] flex flex-col border-gray-300 rounded-xl shadow-md col-span-3 cursor-pointer bg-cover'>
+               <div className=' m-2 rounded-xl backdrop-blur-[4px] bg-white/5'>
+                 <p className='text-4xl font-bold p-2'>Philodendron</p>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit veniam tempora minus dolorum, itaque omnis, ipsa quae et, necessitatibus facilis dignissimos adipisci eos repudiandae inventore sequi. Nesciunt corrupti dolorum debitis.</p>
+                
+               </div>
             </div>
-            <div className='border-1 border-gray-300 rounded-xl shadow-md grid-cols-2'>
-                <p className='text-4xl font-bold'>Philodendron</p>
+            <div className='border-1 border-gray-300 rounded-xl bg-[url("/plant1.jpg")] shadow-md col-span-2 bg-cover text-white  justify-end min-h-[30vh] flex flex-col'>
+                 <div className=' m-2 rounded-xl backdrop-blur-[4px] bg-white/5'>
+                 <p className='text-4xl font-bold p-2'>Philodendron</p>
+                <p> omnis, ipsa quae et, necessitatibus facilis dignissimos adipisci eos repudiandae inventore sequi. Nesciunt corrupti dolorum debitis.</p>
+                
+               </div>
+            </div>
+
+    {/* below  */}
+            <div className='border-1 border-gray-300 rounded-xl shadow-md col-span-2 bg-[url("/plant1.jpg")] bg-cover text-white  justify-end min-h-[30vh] flex flex-col'>
+                <div className=' m-2 rounded-xl backdrop-blur-[4px] bg-white/5'>
+                 <p className='text-4xl font-bold p-2'>Philodendron</p>
+                <p>Loraque omnis, ipsa quae et, necessitatibus facilis dignissimos adipisci eos repudiandae inventore sequi. Nesciunt corrupti dolorum debitis.</p>
+                
+               </div>
+            </div>
+            <div className='border-1 border-gray-300 rounded-xl shadow-md col-span-3 bg-[url("/plant1.jpg")] bg-cover text-white  justify-end min-h-[30vh] flex flex-col    '>
+                <div className=' m-2 rounded-xl backdrop-blur-[4px] bg-white/5'>
+                 <p className='text-4xl font-bold p-2'>Philodendron</p>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit veniam tempora minus dolorum, itaque omnis, ipsa quae et, necessitatibus facilis dignissimos adipisci eos repudiandae inventore sequi. Nesciunt corrupti dolorum debitis.</p>
+                
+               </div>
             </div>
 
         </div>
